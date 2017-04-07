10d9a5878626b43ae745ac44f818b7ad6a9b6015
Karina Kozarova <karinakozarova1303@gmail.com>
2017-04-07 18:32:09 +0300

    Update Ro_code.c

--- Ro_code.c
+++ Ro_code.c
@@ -1,17 +1,24 @@
 #include <kipr/botball.h>
-
-#define LIGHT_SENSOR 1	//light sensor
-#define LINE_SENSOR_PORT 5	//line sensor (tophat)
+//SENSOR PORTS:
+#define LIGHT_SENSOR 1	          //light sensor
+#define LINE_SENSOR_PORT 5	 //line sensor (tophat)
 #define ANALOG_ET_SENSOR_PORT 0	//distance sensor
 
-#define LEFT_WHEEL_PORT 3	//motor 1 
-#define RIGHT_WHEEL_PORT 0	//motor 2
-#define CLIPPER_UP_DOWN_PORT 0	//servo 1
-#define CLIPPER_OPEN_CLOSE_PORT 1 //servo 2
+//MOTOR PORTS:
+#define LEFT_WHEEL_PORT 3	      //motor 1 
+#define RIGHT_WHEEL_PORT 0	     //motor 2
 
+//SERVO PORTS:
+#define CLIPPER_UP_DOWN_PORT 0	    //servo 1
+#define CLIPPER_OPEN_CLOSE_PORT 1  //servo 2
+
+//CONSTS
 #define BLACKLINE_MAX 3000	//blackline velocity
 #define BLACKLINE_MIN 500	//blackline velocity
 
+#define CLOSE_CLIPPERS 1850
+
+//function declaration
 void light_sensor(int LIGHT_SENSOR_PORT );       
 void follow_black_line();
 void near_cup();
@@ -22,20 +29,17 @@ void close_clippers();
 
 /* -------------------------------------------------------------------------------
 			Used ports:
-
 				Sensors:
 				et_sensor = 0 port
 				light sensor = 1 port
 				line_sensor = 5 port
-
 				Motors(wheels):
 				left_wheel = 3 port (gore red, dolu black)
 				right_wheel = 0 port (gore black, dolu red)
-
 				Servos(on clippers) :
 				up-down = 0 port
 				open-close = 1 port
-–-------------------------------------------------------------------------------*/
+-------------------------------------------------------------------------------*/
 
 int main()
 {
@@ -43,7 +47,7 @@ int main()
     while(1) //infinite loop
 	{
 	   near_cup();
-       wait_for_milliseconds(20);  //adds 20ms delay
+           wait_for_milliseconds(20);  //adds 20ms delay
     }
     return 0;
 }
@@ -79,6 +83,7 @@ void near_cup(){
 			ao();	//stops motors
 			close_clippers();
 			bring_servo_up();
+			//TO DO: move forward/backward and open clippers
 		}
 		else follow_black_line();	
 	}
@@ -88,27 +93,28 @@ void bring_servo_up(){
 	enable_servos();
 	//TO DO: write the real code,this probably won't work
 	//WARNING! where it's 2013 maybe it should be 0
-	set_servo_position(CLIPPER_UP_DOWN_PORT,2013);   //(port,angle)
+	set_servo_position(CLIPPER_UP_DOWN_PORT,0);   //(port,angle)
 	disable_servos();
 
 }
 void bring_servo_down(){
 	enable_servos();
 	//TO DO: write the real code
-	set_servo_position(CLIPPER_UP_DOWN_PORT,0);
+	set_servo_position(CLIPPER_UP_DOWN_PORT,2013);
 	disable_servos();
 }
 
 void close_clippers(){
+	//TO BE UPGRADED: the num const could differ
 	enable_servos();
-	set_servo_position(1,1850);
+	set_servo_position(CLIPPER_UP_DOWN_PORT,CLOSE_CLIPPERS);
         printf("Close");
 	disable_servos();
 }
 
 void open_clippers(){
 	enable_servos();
-	//TO DO: write the real code
-	set_servo_position(CLIPPER_OPEN_CLOSE_PORT,1000);
+	//TO BE TESTED
+	set_servo_position(CLIPPER_OPEN_CLOSE_PORT,190);
 	disable_servos();
 }
