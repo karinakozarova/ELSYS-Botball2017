8a7058b6fabfaaeac6ea7a8f7a0c887b61c26142
Karina Kozarova <karinakozarova1303@gmail.com>
2017-04-08 20:46:39 +0300

    Start of April version, final for the day

--- Ro_code.c
+++ Ro_code.c
@@ -30,6 +30,8 @@ void clippers_starting_position();
 void button_reading();
 void backward_movement_3s();
 void forward_movement_3s();
+
+
 /* -------------------------------------------------------------------------------
 		Used ports:
 			Sensors:
@@ -59,20 +61,20 @@ int main()
 	  wait_for_milliseconds(2000);
 	  clippers_down();
   
+		/*	
+					 wait_for_milliseconds(1000);
+						clippers_close();
+						 clippers_open();	
 
-  
-  /*	
- wait_for_milliseconds(1000);
-    clippers_close();
-     clippers_open();	
-
- // clippers_up();
-    while(1) //infinite loop
-	{
-		//follow_black_line();
-    //  button_reading();
-    }
-   */ return 0;
+					 // clippers_up();
+						while(1) //infinite loop
+						{
+							//follow_black_line();
+						//  button_reading();
+						}
+		*/ 
+   
+   return 0;
 }
 
 void light_sensor(int LIGHT_SENSOR_PORT ){
@@ -83,7 +85,7 @@ void light_sensor(int LIGHT_SENSOR_PORT ){
 }
 
 void follow_black_line(){
-
+	//follows black line using the top hat sensor
 	  if(analog(LINE_SENSOR_PORT) < 3000) { 
 	    mav(RIGHT_WHEEL_PORT,BLACKLINE_MIN);
 	    mav(LEFT_WHEEL_PORT,BLACKLINE_MAX);
@@ -99,7 +101,7 @@ void follow_black_line(){
 
 void initial_positions()
 { 
-  	// null position;
+   // null position;
 	set_servo_position(0, 268);
 	set_servo_position(1, 400);
  	enable_servos();
@@ -107,6 +109,7 @@ void initial_positions()
 
 void clippers_up()
 {
+    //moves clippers up
 	printf("Up\n");
 	set_servo_position( CLIPPER_UP_DOWN_PORT, CLIPPERS_MAX);
  	enable_servos();
@@ -114,24 +117,28 @@ void clippers_up()
 
 void clippers_down()
 {
+    //moves clippers down
 	printf("Down\n");
 	set_servo_position( CLIPPER_UP_DOWN_PORT, CLIPPERS_MIN);
  	enable_servos();
 }
 
 void clippers_open(){
-  printf("Open\n");
+     //opens clippers
+        printf("Open\n");
 	set_servo_position(CLIPPER_OPEN_CLOSE_PORT, 500);
  	enable_servos();
 }
 
 void clippers_close(){
+	//closes clippers
   	printf("Close\n");
 	set_servo_position(CLIPPER_OPEN_CLOSE_PORT, 0);
  	enable_servos();
 }
 
 void clippers_starting_position(){
+	//how the clipper should be while going up the ramp
 	set_servo_position(CLIPPER_UP_DOWN_PORT, CLIPPERS_MIN);
 	set_servo_position(CLIPPER_OPEN_CLOSE_PORT, 268);	
 	set_servo_position(CLIPPER_UP_DOWN_PORT, 700);
@@ -142,11 +149,11 @@ void button_reading(){
   //TO BE TESTED!!!!
   if(!digital(Button_Port))  //button is triggered
   {
-      printf("In");
-  	  backward_movement_3s();
-  	  clippers_up();
-   	  follow_black_line();
-   	  clippers_open();
+      	printf("In");
+        backward_movement_3s();
+        clippers_up();
+        follow_black_line();
+        clippers_open();
   }
   else follow_black_line();
   
@@ -157,18 +164,18 @@ void backward_movement_3s(){
 	int i;	
   	for(i=0;i<800;i++) 
    	 {
-    	mav(3,-2500);
+    	        mav(3,-2500);
   	 	mav(0,-2500);
   	  }
     	ao();
   }
 
   void forward_movement_3s(){
-  	//moves backward for 3 seconds
+  	//moves forward for 3 seconds
 	int i;	
   	for(i=0;i<800;i++) 
    	 {
-    	mav(3,2500);
+    		mav(3,2500);
   	 	mav(0,2500);
   	  }
     	ao();
