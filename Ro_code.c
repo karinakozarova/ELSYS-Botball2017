392938eab412ab324d61f5692c42b4623bc89488
Karina Kozarova <karinakozarova1303@gmail.com>
2017-04-08 19:56:54 +0300

    Update Ro_code.c

--- Ro_code.c
+++ Ro_code.c
@@ -3,7 +3,7 @@
 #define LIGHT_SENSOR 1	          //light sensor
 #define LINE_SENSOR_PORT 5	 //line sensor (tophat)
 #define ANALOG_ET_SENSOR_PORT 0	//distance sensor
-
+#define Button_Port 4
 //MOTOR PORTS:
 #define LEFT_WHEEL_PORT 3	      //motor 1 
 #define RIGHT_WHEEL_PORT 0	     //motor 2
@@ -13,19 +13,23 @@
 #define CLIPPER_OPEN_CLOSE_PORT 1  //servo 2
 
 //CONSTS
-#define BLACKLINE_MAX 3000	//blackline velocity
-#define BLACKLINE_MIN 500	//blackline velocity
-
-#define CLOSE_CLIPPERS 1850
+#define BLACKLINE_MAX 2400	//blackline velocity
+#define BLACKLINE_MIN 400	//blackline velocity
+#define CLIPPERS_MAX 2000
+#define CLIPPERS_NULL 0
 
 //function declaration
 void light_sensor(int LIGHT_SENSOR_PORT );       
 void follow_black_line();
-void near_cup();
-void bring_servo_up();
-void bring_servo_down();
-void open_clippers();
-void close_clippers();
+void follow_black_line_backwards();
+void initial_positions();
+void clippers_up();
+void clippers_down();
+void clippers_open();
+void clippers_close();
+void clippers_starting_position();
+
+
 
 /* -------------------------------------------------------------------------------
 			Used ports:
@@ -44,10 +48,14 @@ void close_clippers();
 int main()
 {
     //light_sensor(LIGHT_SENSOR_PORT ); //add this when not testing
+   
+    //clippers_close();
+    initial_positions();
+	clippers_starting_position();
+
     while(1) //infinite loop
 	{
-	   near_cup();
-           wait_for_milliseconds(20);  //adds 20ms delay
+		follow_black_line();
     }
     return 0;
 }
@@ -61,7 +69,7 @@ void light_sensor(int LIGHT_SENSOR_PORT ){
 
 void follow_black_line(){
 
-	  if(analog(LINE_SENSOR_PORT) < 2000) { 
+	  if(analog(LINE_SENSOR_PORT) < 3000) { 
 	    mav(RIGHT_WHEEL_PORT,BLACKLINE_MAX);
 	    mav(LEFT_WHEEL_PORT,BLACKLINE_MIN);
    	    printf("Detected white line...\n");
@@ -74,47 +82,68 @@ void follow_black_line(){
 	  }
 }
 
-void near_cup(){
-	analog_et(ANALOG_ET_SENSOR_PORT); //get a reading
-	while(1)	//infinite loop
-	{
-		if(analog10(ANALOG_ET_SENSOR_PORT) > 512) //is close
-		{
-			ao();	//stops motors
-			close_clippers();
-			bring_servo_up();
-			//TO DO: move forward/backward and open clippers
-		}
-		else follow_black_line();	
-	}
+void initial_positions()
+{
+	set_servo_position(0, 0);
+	set_servo_position(1, 200);
+ 	enable_servos();
 }
 
-void bring_servo_up(){
-	enable_servos();
-	//TO DO: write the real code,this probably won't work
-	//WARNING! where it's 2013 maybe it should be 0
-	set_servo_position(CLIPPER_UP_DOWN_PORT,0);   //(port,angle)
-	disable_servos();
-
-}
-void bring_servo_down(){
-	enable_servos();
-	//TO DO: write the real code
-	set_servo_position(CLIPPER_UP_DOWN_PORT,2013);
-	disable_servos();
+void clippers_up()
+{
+	set_servo_position( CLIPPER_UP_DOWN_PORT, CLIPPERS_MAX);
+ 	enable_servos();
 }
 
-void close_clippers(){
-	//TO BE UPGRADED: the num const could differ
-	enable_servos();
-	set_servo_position(CLIPPER_UP_DOWN_PORT,CLOSE_CLIPPERS);
-        printf("Close");
-	disable_servos();
+void clippers_down()
+{
+	set_servo_position( CLIPPER_UP_DOWN_PORT, CLIPPERS_NULL);
+ 	enable_servos();
 }
 
-void open_clippers(){
-	enable_servos();
-	//TO BE TESTED
-	set_servo_position(CLIPPER_OPEN_CLOSE_PORT,190);
-	disable_servos();
+void clippers_open(){
+	set_servo_position(CLIPPER_OPEN_CLOSE_PORT, CLIPPERS_MAX);
+ 	enable_servos();
+}
+
+void clippers_close(){
+	set_servo_position(CLIPPER_OPEN_CLOSE_PORT, CLIPPERS_NULL);
+ 	enable_servos();
+}
+
+void clippers_starting_position(){
+	set_servo_position(CLIPPER_UP_DOWN_PORT, CLIPPERS_NULL);
+	set_servo_position(CLIPPER_OPEN_CLOSE_PORT, 300);	
+	set_servo_position(CLIPPER_UP_DOWN_PORT, 800);
+ 	enable_servos();
+}
+
+void follow_black_line_backwards(){
+//TO BE TESTED!
+	  if(analog(LINE_SENSOR_PORT) < 3000) { 
+	    mav(RIGHT_WHEEL_PORT,-BLACKLINE_MAX);
+	    mav(LEFT_WHEEL_PORT,-BLACKLINE_MIN);
+   	    printf("Detected white line...\n");
+	  }
+	  else
+	  {
+	    mav(RIGHT_WHEEL_PORT,-BLACKLINE_MIN);
+	    mav(LEFT_WHEEL_PORT, -BLACKLINE_MAX);
+	    printf("Black line...\n");
+	  }
+}
+
+void button_reading(){
+  //TO BE TESTED!!!!
+  if(digital(Button_Port)){
+  //button is triggered
+    int i;
+ 	for(i=0;i<10000;i++) follow_black_line_backwards();
+    clippers_up();
+    for(i=0;i<10000;i++) follow_black_line();
+    clippers_open();
+  }
+  else{
+    //button untouched
+  }
 }
