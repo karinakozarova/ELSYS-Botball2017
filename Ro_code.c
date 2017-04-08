358d300376784944bcb966c7447c09db873528ee
Karina Kozarova <karinakozarova1303@gmail.com>
2017-04-08 19:59:23 +0300

    Update Ro_code.c

--- Ro_code.c
+++ Ro_code.c
@@ -1,22 +1,22 @@
 #include <kipr/botball.h>
 //SENSOR PORTS:
-#define LIGHT_SENSOR 1	          //light sensor
-#define LINE_SENSOR_PORT 5	 //line sensor (tophat)
-#define ANALOG_ET_SENSOR_PORT 0	//distance sensor
-#define Button_Port 4
+#define LIGHT_SENSOR 0	          //light sensor
+#define LINE_SENSOR_PORT 5        //line sensor (tophat)
+#define Button_Port 1
+
 //MOTOR PORTS:
 #define LEFT_WHEEL_PORT 3	      //motor 1 
 #define RIGHT_WHEEL_PORT 0	     //motor 2
 
 //SERVO PORTS:
-#define CLIPPER_UP_DOWN_PORT 0	    //servo 1
-#define CLIPPER_OPEN_CLOSE_PORT 1  //servo 2
+#define CLIPPER_UP_DOWN_PORT 1	    //servo 1
+#define CLIPPER_OPEN_CLOSE_PORT 0  //servo 2
 
 //CONSTS
-#define BLACKLINE_MAX 2400	//blackline velocity
-#define BLACKLINE_MIN 400	//blackline velocity
-#define CLIPPERS_MAX 2000
-#define CLIPPERS_NULL 0
+#define BLACKLINE_MAX 1500	//blackline velocity
+#define BLACKLINE_MIN 500	//blackline velocity
+#define CLIPPERS_MAX 1800
+#define CLIPPERS_NULL 240
 
 //function declaration
 void light_sensor(int LIGHT_SENSOR_PORT );       
@@ -29,35 +29,51 @@ void clippers_open();
 void clippers_close();
 void clippers_starting_position();
 void button_reading();
-
-
+void backward_movement_3s();
 /* -------------------------------------------------------------------------------
 			Used ports:
 				Sensors:
-				et_sensor = 0 port
+				button = 4 port
 				light sensor = 1 port
 				line_sensor = 5 port
 				Motors(wheels):
 				left_wheel = 3 port (gore red, dolu black)
 				right_wheel = 0 port (gore black, dolu red)
 				Servos(on clippers) :
-				up-down = 0 port
-				open-close = 1 port
+				up-down = 1 port
+				open-close = 0 port
 -------------------------------------------------------------------------------*/
 
 int main()
 {
     //light_sensor(LIGHT_SENSOR_PORT ); //add this when not testing
-   
+   	//clippers_up();
     //clippers_close();
-    initial_positions();
-	clippers_starting_position();
 
+  initial_positions();
+  clippers_starting_position();
+ //clippers_up();
+  wait_for_milliseconds(3000);
+ clippers_open();	
+  wait_for_milliseconds(2000);
+ clippers_close();
+  wait_for_milliseconds(2000);
+ clippers_down();
+  
+
+  
+  /*	
+ wait_for_milliseconds(1000);
+    clippers_close();
+     clippers_open();	
+
+ // clippers_up();
     while(1) //infinite loop
 	{
-		follow_black_line();
+		//follow_black_line();
+    //  button_reading();
     }
-    return 0;
+   */ return 0;
 }
 
 void light_sensor(int LIGHT_SENSOR_PORT ){
@@ -70,22 +86,23 @@ void light_sensor(int LIGHT_SENSOR_PORT ){
 void follow_black_line(){
 
 	  if(analog(LINE_SENSOR_PORT) < 3000) { 
-	    mav(RIGHT_WHEEL_PORT,BLACKLINE_MAX);
-	    mav(LEFT_WHEEL_PORT,BLACKLINE_MIN);
+	    mav(RIGHT_WHEEL_PORT,BLACKLINE_MIN);
+	    mav(LEFT_WHEEL_PORT,BLACKLINE_MAX);
    	    printf("Detected white line...\n");
 	  }
 	  else
 	  {
-	    mav(RIGHT_WHEEL_PORT,BLACKLINE_MIN);
-	    mav(LEFT_WHEEL_PORT, BLACKLINE_MAX);
+	    mav(RIGHT_WHEEL_PORT,BLACKLINE_MAX);
+	    mav(LEFT_WHEEL_PORT, BLACKLINE_MIN);
 	    printf("Black line...\n");
 	  }
 }
 
 void initial_positions()
-{
-	set_servo_position(0, 0);
-	set_servo_position(1, 200);
+{ 
+  // null position
+	set_servo_position(0, 268);
+	set_servo_position(1, 400);
  	enable_servos();
 }
 
@@ -102,48 +119,45 @@ void clippers_down()
 }
 
 void clippers_open(){
-	set_servo_position(CLIPPER_OPEN_CLOSE_PORT, CLIPPERS_MAX);
+  printf("Open");
+	set_servo_position(CLIPPER_OPEN_CLOSE_PORT, 500);
  	enable_servos();
 }
 
 void clippers_close(){
-	set_servo_position(CLIPPER_OPEN_CLOSE_PORT, CLIPPERS_NULL);
+  printf("close");
+	set_servo_position(CLIPPER_OPEN_CLOSE_PORT, 0);
  	enable_servos();
 }
 
 void clippers_starting_position(){
 	set_servo_position(CLIPPER_UP_DOWN_PORT, CLIPPERS_NULL);
-	set_servo_position(CLIPPER_OPEN_CLOSE_PORT, 300);	
-	set_servo_position(CLIPPER_UP_DOWN_PORT, 800);
+	set_servo_position(CLIPPER_OPEN_CLOSE_PORT, 268);	
+	set_servo_position(CLIPPER_UP_DOWN_PORT, 700);
  	enable_servos();
-}
-
-void follow_black_line_backwards(){
-//TO BE TESTED!
-	  if(analog(LINE_SENSOR_PORT) < 3000) { 
-	    mav(RIGHT_WHEEL_PORT,-BLACKLINE_MAX);
-	    mav(LEFT_WHEEL_PORT,-BLACKLINE_MIN);
-   	    printf("Detected white line...\n");
-	  }
-	  else
-	  {
-	    mav(RIGHT_WHEEL_PORT,-BLACKLINE_MIN);
-	    mav(LEFT_WHEEL_PORT, -BLACKLINE_MAX);
-	    printf("Black line...\n");
-	  }
 }
 
 void button_reading(){
   //TO BE TESTED!!!!
-  if(digital(Button_Port)){
-  //button is triggered
-    int i;
- 	for(i=0;i<10000;i++) follow_black_line_backwards();
-    clippers_up();
-    for(i=0;i<10000;i++) follow_black_line();
-    clippers_open();
+  if(!digital(Button_Port))
+  {
+ 	 //button is triggered
+      printf("In");
+  	  backward_movement_3s();
+  	  clippers_up();
+   	  follow_black_line();
+   	  clippers_open();
   }
-  else{
-    //button untouched
-  }
+  else follow_black_line();
+  
 }
+  
+void backward_movement_3s(){
+  	int i;	
+  	for(i=0;i<800;i++) 
+    {
+    		mav(3,-2500);
+  		 	mav(0,-2500);
+    }
+    ao();
+  }
