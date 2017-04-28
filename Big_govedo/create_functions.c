70e70db1886809c7383a850e47442c3935356349
Владо <vladimir.garistov42@gmail.com>
2017-04-28 10:54:18 +0300

    Update create_functions.c

--- Big_govedo/create_functions.c
+++ Big_govedo/create_functions.c
@@ -12,10 +12,10 @@ int create_init()
 	{
 		enable_servos();
 		set_servo_position(LIFT_SERVO, CLAW_HIGH);
-		set_servo_position(CLAW_SERVO, CLAW_OPEN);
+		set_servo_position(CLAW_SERVO, CLAW_CLOSED);
 		msleep(WAIT_FOR_SERVO);
-		//wait_for_light(STARTER);
-		shut_down_in(120);
+		wait_for_light(STARTER);
+		shut_down_in(119);
 		return 0;
 	}
 	else
@@ -46,7 +46,7 @@ void create_turn_right()
 		create_drive_direct(TURN_SPEED, -TURN_SPEED);
 		msleep(5);
 	}
-	msleep(11);
+	msleep(12);
 	create_stop();
 }
 
@@ -58,7 +58,7 @@ void create_turn_left()
 		create_drive_direct(-TURN_SPEED, TURN_SPEED);
 		msleep(5);
 	}
-	msleep(15);
+	msleep(12);
 	create_stop();
 }
 
@@ -101,7 +101,7 @@ void place_bale()
 	int x=0;
 	create_forward(BACK_OFF);
 	create_turn_angle(180);
-	create_forward(1.4*PIPE_BUMP);
+	create_forward((int)(1.8*PIPE_BUMP));		//(int)
 	create_backward(BACK_OFF + bales_placed*CLAW_DISPLACEMENT);
 	create_turn_right();
 	while(analog(LINE_SENSOR) < BLACK)
@@ -135,39 +135,61 @@ void place_bale()
 void get_last_bale()
 {
 	create_drive_direct(200, 200);
-	move_to_nth_bale(4);
+	move_to_nth_bale(2);	//3?
 	//create_backward(EARLY_BALE_CORRECTION);
+	//create_backward(LAST_BALE);
+    create_turn_right();
+    set_servo_position(CLAW_SERVO, CLAW_MIDDLE);
+    msleep(WAIT_FOR_SERVO);
+    set_servo_position(CLAW_SERVO, CLAW_LOW);
+    msleep(WAIT_FOR_SERVO);
+    create_turn_right();
+    set_servo_position(CLAW_SERVO, CLAW_HIGH);
+    msleep(WAIT_FOR_SERVO);
+    create_turn_left();
+    create_turn_left();
+    move_to_nth_bale(1);
 	grab_second_bale();
 }
 
 void go_to_bales()
 {
-	create_forward(PIPE_BUMP/2);
+	create_forward(PIPE_BUMP);
 	create_backward(BACK_OFF);
-	create_turn_right();
+	/*create_turn_right();
 	create_drive_direct(-TURN_SPEED, TURN_SPEED);
 	msleep(150);
 	create_stop();
+	create_turn_right();*/
+	create_backward(DO_BALITE);
+	set_servo_position(LIFT_SERVO, CLAW_HIGH);
 	create_turn_right();
-	create_forward(DO_BALITE);
-	create_turn_left();
-	create_forward(1.4*PIPE_BUMP);
-	create_backward(1.1*BACK_OFF);	//+
+	create_forward((int)(1.8*PIPE_BUMP));		//(int)
+	create_backward((int)(1.1*BACK_OFF));		//(int)
 	create_turn_left();
 }
 
 void get_more_bales()
 {
 	create_backward(RETURN_TO_BALES);
-	create_turn_right();
+	create_turn_left();
 	msleep(500);
+    create_drive_direct(-200, -200);
+    msleep(1500);
+   
+    create_drive_direct(200, 200);
+    msleep(BACK_OFF);
+    
+    create_turn_right();
+    create_turn_right();
+    
 	create_drive_direct(-200, -200);
-	move_to_nth_bale(1);
-	//create_forward(EARLY_BALE_CORRECTION);
+    move_to_nth_bale(1);
+	create_forward(EARLY_BALE_CORRECTION);
 	grab_bale();
 	create_drive_direct(-200, -200);
 	move_to_nth_bale(1);
-	//create_forward(EARLY_BALE_CORRECTION);
+	create_forward(EARLY_BALE_CORRECTION);
 	create_drive_direct(200, 200);
 	msleep(BALE_CATCH_CORRECTION);
 	create_stop();
@@ -177,19 +199,25 @@ void get_more_bales()
 
 void move_to_nth_bale(int n)
 {
-	char bales_detected=0;
+  	printf("Moving the the %d bale now", n);
+	int bales_detected=0;
 	int prev_value=0;
 	int current_value=0;
 	while(bales_detected < n)
 	{	
 		current_value=analog(BALE_SENSOR);
 		printf("%d\n", current_value);
-		if(current_value > BALE_DETECTED && prev_value < BALE_DETECTED)
+		if(current_value > BALE_DETECTED+200 && prev_value < BALE_DETECTED+200)
 		{
-			printf("\n++++++++++++++++++");
+            if(debounceSensor(BALE_SENSOR, 20) <= BALE_DETECTED+200)
+                return;
+            
+			//printf("\n++++++++++++++++++");
 			bales_detected++;
+          	printf("Bale detected. Current bale : %d", bales_detected);
 		}
-		prev_value=current_value;
+        
+		prev_value=current_value;      	
 		msleep(10);
 	}
 	create_stop();
@@ -204,7 +232,7 @@ void grab_second_bale()
 	set_servo_position(CLAW_SERVO, CLAW_OPEN);
 	msleep(WAIT_FOR_SERVO);
 	if(bales_placed==0)
-		create_backward(50);
+		create_forward(50);
 	set_servo_position(LIFT_SERVO, CLAW_LOW);
 	msleep(WAIT_FOR_SERVO);
 	set_servo_position(CLAW_SERVO, CLAW_CLOSED);
@@ -228,4 +256,16 @@ void get_the_bowl()
 	create_turn_right();
 	create_backward(2500);
 	create_turn_left();
+}
+
+void few_more_points()
+{
+  	create_forward(GET_IN_BARN);
+  	create_turn_right();
+}
+
+int debounceSensor(int pin, int time){
+	msleep(time);
+    
+    return analog(pin);
 }
