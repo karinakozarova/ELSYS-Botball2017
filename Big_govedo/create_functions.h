708d729c159ed110d36b5ac31fde0d48f0f47bf5
Владо <vladimir.garistov42@gmail.com>
2017-04-28 10:54:37 +0300

    Update create_functions.h

--- Big_govedo/create_functions.h
+++ Big_govedo/create_functions.h
@@ -1,11 +1,9 @@
-//Портове
 #define CLAW_SERVO 2
 #define LIFT_SERVO 1
 #define STARTER 4
 #define BALE_SENSOR 5
 #define LINE_SENSOR 3
 
-//Серво мотори
 #define CLAW_OPEN 1605
 #define CLAW_CLOSED 680
 #define CLAW_LOW 1400
@@ -16,22 +14,21 @@
 const int release[4]={1350, 1200, 1070, 900};
 #endif
 
-//Сензори
-#define BALE_DETECTED 2600	//2600
+#define BALE_DETECTED 2100	//2600
 #define BLACK 3500
 
-//Разстояния
 #define PIPE_BUMP 800
 #define BACK_OFF 400
-#define DO_BALITE 4450	//Зависи от батерията
-#define RETURN_TO_BALES 950
-#define AFTER_BALES 1100	//Зависи от батерията
+#define DO_BALITE 4200
+#define RETURN_TO_BALES 1000
+#define AFTER_BALES 1100
 #define GET_IN_BARN 850
+#define PUSH_FIRST_BALE 400
+#define LAST_BALE 300
 
-//Корекции
-#define TURN_CORRECTION 1.046	//0.075 Зависи от разположението на контролера върху робота
-#define CLAW_DISPLACEMENT -20	//++
-#define _90_DEGREES 29	//презавива
+#define TURN_CORRECTION 1.046
+#define CLAW_DISPLACEMENT 50
+#define _90_DEGREES 29
 #define TURN_SPEED 200
 #define WAIT_FOR_SERVO 700
 #define BALE_CATCH_CORRECTION 50
@@ -52,3 +49,5 @@ void get_more_bales();
 void move_to_nth_bale(int n);
 void grab_second_bale();
 void get_the_bowl();
+void few_more_points();
+int debounceSensor(int pin, int time);
